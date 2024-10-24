8121ca5ba32b07a1c42a02efed20e8da4e40f8b4
Edinho Monteiro <bruno.monteirodg@gmail.com>
2024-10-24 18:06:17 -0300

    feat: add environment variables

--- importer-api/src/config/settings.py
+++ importer-api/src/config/settings.py
@@ -4,7 +4,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    aws_region: str = getenv("AWS_REGION", "us-east-1")
     log_level: str = getenv("LOG_LEVEL", "INFO")
+    sqs_endpoint_url: str = getenv("SQS_ENDPOINT_URL")
+    sqs_queue_url: str = getenv("SQS_QUEUE_URL")
 
 
 @lru_cache()
