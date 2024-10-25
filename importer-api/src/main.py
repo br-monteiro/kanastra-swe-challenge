6fa9fcdcacca650c0b0250f7914eaa62f7a1809a
Edinho Monteiro <bruno.monteirodg@gmail.com>
2024-10-25 05:40:18 -0300

    feat: use importer route

--- importer-api/src/main.py
+++ importer-api/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from src.api.health_check.routes import router as health_check_router
+from src.api.importer_file.routes import router as importer_router
 from src.api.metrics.routes import router as metrics_router
 
 
@@ -7,4 +8,5 @@ app = FastAPI()
 
 
 app.include_router(health_check_router)
+app.include_router(importer_router)
 app.include_router(metrics_router)
