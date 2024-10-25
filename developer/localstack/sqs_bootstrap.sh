650562835563dc69676240ff3a5a682f05cdc7bd
Edinho Monteiro <bruno.monteirodg@gmail.com>
2024-10-25 20:11:53 -0300

    refactor: create AWS infra on localstack

--- developer/localstack/sqs_bootstrap.sh
+++ developer/localstack/sqs_bootstrap.sh
@@ -1,17 +1,30 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -euo pipefail
+apt install jq -y
+
+echo "Creating SQS queues"
+awslocal sqs create-queue --queue-name data-process
+awslocal sqs create-queue --queue-name data-process-dlq
+awslocal sqs create-queue --queue-name process-email
 
 
-echo "configuring sqs"
-echo "==================="
+echo "Creating SNS topic"
+awslocal sns create-topic --name data-process-topic
 
-LOCALSTACK_HOST=localhost
-AWS_REGION=us-east-1
 
-create_queue() {
-    local QUEUE_NAME_TO_CREATE=$1
-    awslocal --endpoint-url=http://${LOCALSTACK_HOST}:4566 sqs create-queue --queue-name ${QUEUE_NAME_TO_CREATE} --region ${AWS_REGION} --attributes VisibilityTimeout=30
-}
+data_process_queue_url=$(awslocal sqs get-queue-url --queue-name data-process | jq -r '.QueueUrl')
+data_process_dlq_arn=$(awslocal sqs get-queue-attributes --queue-url $data_process_queue_url --attribute-names QueueArn | jq -r '.Attributes.QueueArn')
+data_process_topic_arn=$(awslocal sns list-topics | jq -r '.Topics[] | select(.TopicArn | contains("data-process-topic")).TopicArn')
 
-create_queue "data-process"
+
+echo "Config queue data-process"
+awslocal sqs set-queue-attributes --queue-url $data_process_queue_url --attributes RedrivePolicy='{"deadLetterTargetArn":"'$data_process_dlq_arn'","maxReceiveCount":"5"}'
+
+
+echo "Subscribe process-email to data-process-topic"
+process_email_queue_url=$(awslocal sqs get-queue-url --queue-name process-email | jq -r '.QueueUrl')
+process_email_arn=$(awslocal sqs get-queue-attributes --queue-url $process_email_queue_url --attribute-names QueueArn | jq -r '.Attributes.QueueArn')
+awslocal sns subscribe --topic-arn $data_process_topic_arn --protocol sqs --notification-endpoint $process_email_arn
+
+
+echo "finish"
