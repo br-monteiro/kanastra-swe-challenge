b322df9e7de92188eb93be053634b693c2d66a5e
Edinho Monteiro <bruno.monteirodg@gmail.com>
2024-10-25 05:42:01 -0300

    feat: add cvs processor

---
+++ importer-api/src/processor/csv_processor.py
@@ -0,0 +1,44 @@
+import asyncio
+from fastapi import UploadFile
+from src.aws.sqs.sqs_client import SQSClient
+from src.config.settings import Settings
+
+class CSVProcessor:
+    def __init__(self, settings: Settings, upload_file: UploadFile, sqs_client: SQSClient):
+        self._file_header = None
+        self._chunk_size = 1048576 # 1024 * 1024 = 1MB
+        self.settings = settings
+        self.upload_file = upload_file
+        self.sqs_client = sqs_client
+
+    async def process(self):
+        messages = []
+        semaphore = asyncio.Semaphore(self.settings.max_csv_process_concurrent_tasks)
+
+        while True:
+            chunk = await self.upload_file.read(self._chunk_size)
+            if not chunk:
+                break
+
+            lines = chunk.decode().splitlines()
+            for line in lines:
+                normalized_line = line.strip()
+                messages.append(normalized_line)
+
+                if self._is_message_buffer_full(messages):
+                    await self._send_batch_messages(semaphore, messages)
+                    messages = []
+
+        if messages:
+            await self._send_batch_messages(semaphore, messages)
+
+    def _is_message_buffer_full(self, messages: list):
+        return len(messages) >= self.settings.max_sqs_send_message_batch_size
+
+    async def _send_batch_messages(self, semaphore: asyncio.Semaphore, messages: list):
+        async with semaphore:
+            try:
+                await self.sqs_client.send_message_batch(messages)
+            except Exception as e:
+                print(f"Erro ao enviar mensagens: {e}")
+
