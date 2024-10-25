8985bb8d425b3cf52c7b49e37998072e0dd34f58
Edinho Monteiro <bruno.monteirodg@gmail.com>
2024-10-25 14:25:51 -0300

    refactor: change csvprocessor implementation and add custom metrics

--- importer-api/src/processor/csv_processor.py
+++ importer-api/src/processor/csv_processor.py
@@ -1,19 +1,22 @@
 import asyncio
-from fastapi import UploadFile
 from src.aws.sqs.sqs_client import SQSClient
 from src.config.settings import Settings
 from src.logger.logger import get_logger
+from src.metrics.metrics_registry_manager import get_metrics_registry
 
+METRICS = get_metrics_registry()
+METRICS.register_counter("csv_processor_messages_sent", "Number of messages sent to SQS")
+METRICS.register_counter("csv_processor_messages_failed", "Number of messages failed to send to SQS")
+METRICS.register_summary("csv_processor_duration_seconds", "Duration of CSV processing in seconds")
 
 class CSVProcessor:
-    def __init__(self, settings: Settings, upload_file: UploadFile, sqs_client: SQSClient):
-        self._file_header = None
-        self._chunk_size = 1048576  # 1024 * 1024 = 1MB
+    def __init__(self, settings: Settings, file_content: bytes, sqs_client: SQSClient):
         self.settings = settings
-        self.upload_file = upload_file
+        self.file_content = file_content
         self.sqs_client = sqs_client
         self.logger = get_logger(__name__)
 
+    @METRICS.get("csv_processor_duration_seconds").time()
     async def process(self):
         self.logger.debug("Initiating CSV processing")
         messages = []
@@ -21,19 +24,16 @@ class CSVProcessor:
             self.settings.max_csv_process_concurrent_tasks
         )
 
-        while True:
-            chunk = await self.upload_file.read(self._chunk_size)
-            if not chunk:
-                break
+        lines = self.file_content.decode().splitlines()
 
-            lines = chunk.decode().splitlines()
-            for line in lines:
-                normalized_line = line.strip()
-                messages.append(normalized_line)
+        for line in lines:
+            normalized_line = line.strip()
+            messages.append(normalized_line)
 
-                if self._is_message_buffer_full(messages):
-                    await self._send_batch_messages(semaphore, messages)
-                    messages = []
+            if self._is_message_buffer_full(messages):
+                await self._send_batch_messages(semaphore, messages)
+                messages = []
+
 
         if messages:
             await self._send_batch_messages(semaphore, messages)
@@ -42,11 +42,10 @@ class CSVProcessor:
         return len(messages) >= self.settings.max_sqs_send_message_batch_size
 
     async def _send_batch_messages(self, semaphore: asyncio.Semaphore, messages: list):
-        self.logger.debug(f"Sending {len(messages)} messages to SQS")
-
         async with semaphore:
             try:
-                await self.sqs_client.send_message_batch(messages)
+                self.sqs_client.send_message_batch(messages)
+                METRICS.get("csv_processor_messages_sent").inc(len(messages))
             except Exception as e:
                 self.logger.error(f"Error sending messages: {e}")
-                print(f"Erro ao enviar mensagens: {e}")
+                METRICS.get("csv_processor_messages_failed").inc(len(messages))
