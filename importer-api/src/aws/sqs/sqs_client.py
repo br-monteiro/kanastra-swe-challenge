4ac694e6896b414dc417c1368026d017977e893c
Edinho Monteiro <bruno.monteirodg@gmail.com>
2024-10-25 05:33:46 -0300

    refactor: change sqs client to be able to send batch messages

--- importer-api/src/aws/sqs/sqs_client.py
+++ importer-api/src/aws/sqs/sqs_client.py
@@ -5,8 +5,9 @@ from src.aws.sqs.exceptions.sqs_client_exception import SQSClientException
 
 
 class SQSClient:
-    def __init__(self, settings: Settings):
+    def __init__(self, queue_url: str, settings: Settings):
         self._client = None
+        self.queue_url = queue_url
         self.settings = settings
         self.logger = get_logger(__name__)
 
@@ -18,15 +19,53 @@ class SQSClient:
         )
         self.logger.info("SQS client created")
 
-    def send_message(self, queue_url: str, message_body: str):
+    def send_message(self, message_body: str):
         self._validate_client()
-        self._client.send_message(QueueUrl=queue_url, MessageBody=message_body)
-        self.logger.debug(
-            f"Message sent to queue {queue_url}",
-            extra={
-                "message_body": message_body
-            }
-        )
+
+        try:
+            self._client.send_message(
+                QueueUrl=self.queue_url, MessageBody=message_body)
+            self.logger.debug(
+                f"Message sent to queue {self.queue_url}",
+                extra={
+                    "message_body": message_body
+                }
+            )
+        except Exception as e:
+            self.logger.error(
+                f"Error sending message to queue {self.queue_url}",
+                extra={
+                    "message_body": message_body,
+                    "error": str(e)
+                }
+            )
+
+    def send_message_batch(self, messages: list):
+        self._validate_client()
+
+        entries = [
+            {"Id": str(i), "MessageBody": message} for i, message in enumerate(messages)
+        ]
+
+        try:
+            self._client.send_message_batch(
+                QueueUrl=self.queue_url,
+                Entries=entries
+            )
+            self.logger.debug(
+                f"Messages sent to queue {self.queue_url}",
+                extra={
+                    "messages": messages
+                }
+            )
+        except Exception as e:
+            self.logger.error(
+                f"Error sending messages to queue {self.queue_url}",
+                extra={
+                    "messages": messages,
+                    "error": str(e)
+                }
+            )
 
     def _validate_client(self):
         if not self._client:
