4058e4e0b68d6bbbb4f4e258b16c16644c1605f4
Edinho Monteiro <bruno.monteirodg@gmail.com>
2024-10-25 17:52:32 -0300

    test: add new routes into main test

--- importer-api/tests/test_main.py
+++ importer-api/tests/test_main.py
@@ -1,6 +1,8 @@
 from fastapi.testclient import TestClient
 from src.main import app
+from src.api.file_importer.routes import router as file_importer_router
 from src.api.health_check.routes import router as health_check_router
+from src.api.metrics.routes import router as metrics_router
 
 client = TestClient(app)
 
@@ -11,4 +13,10 @@ def test_app_creation():
 
 def test_included_routers():
     for route in health_check_router.routes:
+        assert route in app.routes
+
+    for route in metrics_router.routes:
+        assert route in app.routes
+
+    for route in file_importer_router.routes:
         assert route in app.routes
