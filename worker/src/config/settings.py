98424963bf4d5780260ac6777d0b3ea680e20ef0
Edinho Monteiro <bruno.monteirodg@gmail.com>
2024-10-25 22:23:29 -0300

    feat: add new environment variables

--- worker/src/config/settings.py
+++ worker/src/config/settings.py
@@ -5,6 +5,11 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     log_level: str = getenv("LOG_LEVEL", "INFO")
+    aws_region: str = getenv("AWS_REGION", "us-east-1")
+    sqs_queue_url: str = getenv("SQS_QUEUE_URL", "")
+    sqs_endpoint_url: str = getenv("SQS_ENDPOINT_URL", "")
+    sqs_max_messages: int = int(getenv("SQS_MAX_MESSAGES", 10))
+    sqs_wait_time_seconds: int = int(getenv("SQS_WAIT_TIME_SECONDS", 20))
 
 
 @lru_cache()
