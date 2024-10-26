ee5085f6ab1d456cb8f8d3fd2c8595402d62891c
Edinho Monteiro <bruno.monteirodg@gmail.com>
2024-10-25 22:56:35 -0300

    test: add tests for SNS implementation

---
+++ worker/tests/aws/sns/test_sns_client.py
@@ -0,0 +1,91 @@
+import pytest
+from unittest.mock import MagicMock, patch
+from src.aws.sns.sns_client import SNSClient
+
+
+@pytest.fixture
+def settings():
+    _settings = MagicMock()
+    _settings.aws_region = "us-east-1"
+    _settings.sns_endpoint_url = "http://localhost:4566"
+    return _settings
+
+
+@patch("src.aws.sns.sns_client.get_logger")
+def test_init(get_logger, settings):
+    sns_client = SNSClient(
+        "arn:aws:sns:us-east-1:123456789012:topic", settings)
+
+    assert sns_client._client is None
+    assert sns_client.topic_arn == "arn:aws:sns:us-east-1:123456789012:topic"
+    assert sns_client.settings == settings
+    get_logger.assert_called_once_with("src.aws.sns.sns_client")
+
+
+@patch("src.aws.sns.sns_client.boto3.client")
+@patch("src.aws.sns.sns_client.get_logger")
+def test_create_client(get_logger, boto3_client, settings):
+    sns_client = SNSClient(
+        "arn:aws:sns:us-east-1:123456789012:topic", settings)
+    sns_client.create_client()
+
+    boto3_client.assert_called_once_with(
+        "sns",
+        region_name="us-east-1",
+        endpoint_url="http://localhost:4566"
+    )
+    assert sns_client._client == boto3_client.return_value
+
+
+@patch("src.aws.sns.sns_client.get_logger")
+def test_publish(get_logger, settings):
+    sns_client = SNSClient(
+        "arn:aws:sns:us-east-1:123456789012:topic", settings)
+    sns_client._client = MagicMock()
+    sns_client.publish("Kanastra")
+
+    sns_client._client.publish.assert_called_once_with(
+        TopicArn="arn:aws:sns:us-east-1:123456789012:topic",
+        Message="Kanastra"
+    )
+    get_logger.return_value.debug.assert_called_once_with(
+        "Message published to topic arn:aws:sns:us-east-1:123456789012:topic",
+        extra={"content": "Kanastra"}
+    )
+
+
+@patch("src.aws.sns.sns_client.get_logger")
+def test_publish_no_client(get_logger, settings):
+    sns_client = SNSClient(
+        "arn:aws:sns:us-east-1:123456789012:topic", settings)
+
+    with pytest.raises(Exception) as exc:
+        sns_client.publish("Kanastra")
+
+    assert str(exc.value) == "SNS client not created"
+    get_logger.return_value.error.assert_called_once_with(
+        "SNS client not created")
+
+
+@patch("src.aws.sns.sns_client.get_logger")
+def test_validate_client(get_logger, settings):
+    sns_client = SNSClient(
+        "arn:aws:sns:us-east-1:123456789012:topic", settings)
+    sns_client._client = MagicMock()
+
+    sns_client._validate_client()
+
+    get_logger.return_value.error.assert_not_called()
+
+
+@patch("src.aws.sns.sns_client.get_logger")
+def test_validate_client_no_client(get_logger, settings):
+    sns_client = SNSClient(
+        "arn:aws:sns:us-east-1:123456789012:topic", settings)
+
+    with pytest.raises(Exception) as exc:
+        sns_client._validate_client()
+
+    assert str(exc.value) == "SNS client not created"
+    get_logger.return_value.error.assert_called_once_with(
+        "SNS client not created")
