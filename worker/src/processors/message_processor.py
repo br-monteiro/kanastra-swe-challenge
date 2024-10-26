d76ee8e57b40dcfaca9635cdddd331c48cad7650
Edinho Monteiro <bruno.monteirodg@gmail.com>
2024-10-26 14:03:33 -0300

    feat: add message processor (#13)
    * fix: change app name in metrics
    
    * feat: add message processor
    
    * test: add test for message processor

---
+++ worker/src/processors/message_processor.py
@@ -0,0 +1,27 @@
+from src.aws.sqs.sqs_consumer import SQSConsumer
+from src.logger.logger import get_logger
+from src.handlers.handler import Handler
+from src.metrics.metrics_registry_manager import get_metrics_registry
+
+
+METRICS = get_metrics_registry()
+METRICS.register_counter("messages_processed_successfully", "Messages processed successfully")
+METRICS.register_counter("messages_processed_errors", "Messages processed with errors")
+
+
+class MessageProcessor:
+    def __init__(self, handler: Handler, sqs_consumer: SQSConsumer):
+        self.handler = handler
+        self.sqs_consumer = sqs_consumer
+        self.logger = get_logger(__name__)
+
+    def process(self):
+        self.logger.debug("Starting message processing")
+        for message in self.sqs_consumer.consume():
+            try:
+                self.logger.debug(f"Processing message: {message.body}")
+                self.handler.handle(message)
+                METRICS.get("messages_processed_successfully").inc()
+            except Exception as e:
+                self.logger.error(f"Error processing message: {e}")
+                METRICS.get("messages_processed_errors").inc()
---
+++ worker/tests/processor/test_message_processor.py
@@ -0,0 +1,69 @@
+import pytest
+from unittest.mock import MagicMock, patch
+from src.processors.message_processor import MessageProcessor
+
+
+@pytest.fixture
+def handler():
+    return MagicMock()
+
+
+@pytest.fixture
+def sqs_consumer():
+    return MagicMock()
+
+
+@patch("src.processors.message_processor.get_logger")
+def test_init(get_logger, handler, sqs_consumer):
+    message_processor = MessageProcessor(handler, sqs_consumer)
+
+    assert message_processor.handler == handler
+    assert message_processor.sqs_consumer == sqs_consumer
+    get_logger.assert_called_once_with("src.processors.message_processor")
+
+
+@patch("src.processors.message_processor.METRICS")
+@patch("src.processors.message_processor.get_logger")
+def test_process(get_logger, metrics, handler, sqs_consumer):
+    message1 = MagicMock(body="message1")
+    message2 = MagicMock(body="message2")
+    sqs_consumer.consume.return_value = [message1, message2]
+
+    message_processor = MessageProcessor(handler, sqs_consumer)
+    message_processor.process()
+
+    sqs_consumer.consume.assert_called_once()
+    handler.handle.assert_any_call(message1)
+    handler.handle.assert_any_call(message2)
+    get_logger.return_value.debug.assert_any_call(
+        "Starting message processing")
+    get_logger.return_value.debug.assert_any_call(
+        "Processing message: message1")
+    get_logger.return_value.debug.assert_any_call(
+        "Processing message: message2")
+    get_logger.return_value.error.assert_not_called()
+    metrics.get("messages_processed_successfully").inc.assert_any_call()
+
+@patch("src.processors.message_processor.METRICS")
+@patch("src.processors.message_processor.get_logger")
+def test_process_error(get_logger, metrics, handler, sqs_consumer):
+    message1 = MagicMock(body="message1")
+    message2 = MagicMock(body="message2")
+    sqs_consumer.consume.return_value = [message1, message2]
+    handler.handle.side_effect = Exception("error")
+
+    message_processor = MessageProcessor(handler, sqs_consumer)
+    message_processor.process()
+
+    sqs_consumer.consume.assert_called_once()
+    handler.handle.assert_any_call(message1)
+    handler.handle.assert_any_call(message2)
+    get_logger.return_value.debug.assert_any_call(
+        "Starting message processing")
+    get_logger.return_value.debug.assert_any_call(
+        "Processing message: message1")
+    get_logger.return_value.debug.assert_any_call(
+        "Processing message: message2")
+    get_logger.return_value.error.assert_any_call(
+        "Error processing message: error")
+    metrics.get.assert_called_with("messages_processed_errors")
